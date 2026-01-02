2cfff2ac0b916523f239d6f8c92318fab6c0fdec
Manoj-M19 <manojpavithra2004@gmail.com>
2026-01-02 18:38:22 +0000

    modified

--- src/components/entity-components.tsx
+++ src/components/entity-components.tsx
@@ -1,3 +1,8 @@
+import {  PlusIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+
 type EntityHeaderProps = {
     title:string ;
     description?:string;
@@ -24,7 +29,32 @@ export const EntityHeader = ({
         <div className="flex flex-row items-center justify-between gap-x-4">
             <div className="flex flex-col">
                 <h1 className="text-lg md:text-xl font-semibold">{title}</h1>
+                {description && (
+                    <p className="text-xs md:text-sm text-muted-foreground">
+                        {description}
+                    </p>
+                )}
             </div>
+            {onNew && !newButtonHref && (
+                <Button
+                disabled = {isCreating || disabled}
+                size="sm"
+                onClick={onNew}
+                >
+                    <PlusIcon className="size-4"/>
+                    {newButtonLabel}
+                </Button>
+            )}
+            {newButtonHref && !onNew && (
+                <Button
+                size="sm"
+                asChild
+                >
+                    <Link href={newButtonHref} prefetch>
+                    <PlusIcon className="size-4"/>
+                    {newButtonLabel}</Link>
+                </Button>
+            )}
         </div>
     )
 }
